2c722f6f0f69a8c8009158603443127aceebece0
ay-dx <moppy_jpn@yahoo.co.jp>
2026-06-12 11:17:41 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import List from './components/List';
 import Detail from './components/Detail';
+import Metrics from './components/Metrics';
+import Faq from './components/Faq';
 
 export default function App() {
   return (
     <HashRouter>
       <div className="w-full max-w-[480px] h-[100dvh] mx-auto bg-cream relative overflow-hidden shadow-2xl flex flex-col">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/list" element={<List />} />
-          <Route path="/detail" element={<Detail />} />
-        </Routes>
+        <Header />
+        <div className="flex-1 overflow-y-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/metrics" element={<Metrics />} />
+            <Route path="/faq" element={<Faq />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </HashRouter>
   );
