8418e5f0dec74122036783a2fcfe0f3dddbd3dca
ay-dx <moppy_jpn@yahoo.co.jp>
2026-06-14 01:57:16 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ function PageTracker() {
   const location = useLocation();
 
   useEffect(() => {
-    if (typeof window.gtag === 'function') {
-      window.gtag('event', 'page_view', {
-        page_path: location.pathname + location.search,
-        page_title: document.title,
-        page_location: window.location.href,
-      });
-    }
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'page_view',
+      page_path: location.pathname + location.search,
+      page_title: document.title,
+      page_location: window.location.href,
+    });
   }, [location]);
 
   return null;
